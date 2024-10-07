efd46a3afaa17f41c4920ea2f2b38c7fed45bda8
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-10-08 03:17:23 +0530

    cors updated

--- index.js
+++ index.js
@@ -23,10 +23,7 @@ dbConnect()
 app.use(express.json())
 app.use(cookieParser())
 app.use(
-    cors({
-        origin:"http://localhost:5173",
-        credentials:true,
-    })
+    cors()
 )
 
 app.use(fileUpload({
